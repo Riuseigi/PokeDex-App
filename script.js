63372397094175543c11e01415614293b3a9a128
Riuseigi <troyliamjarata@gmail.com>
2024-02-11 16:26:24 +0800

    able to make responsive the homepage

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.navLinks').classList.toggle('active');
+    document.querySelector('.hamburger').classList.toggle('active');
+  });
+  
