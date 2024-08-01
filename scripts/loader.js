1a8ad73b566afa458db9e5d7a94bc9cdb9fccb88
Riuseigi <troyliamjarata@gmail.com>
2024-08-01 15:26:54 +0800

    add loader

---
+++ scripts/loader.js
@@ -0,0 +1,15 @@
+const loader = document.getElementById("loader");
+
+/**
+ * Display the loader element.
+ */
+export function showLoader() {
+  loader.style.display = "block";
+}
+
+/**
+ * Hide the loader element.
+ */
+export function hideLoader() {
+  loader.style.display = "none";
+}
