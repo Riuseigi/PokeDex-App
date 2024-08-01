6b7e008510696298057171afea0539820d640cbf
Riuseigi <troyliamjarata@gmail.com>
2024-08-01 15:23:51 +0800

    remove Error image

--- scripts/fetchDataUrl.js
+++ scripts/fetchDataUrl.js
@@ -1,21 +1,27 @@
-import { displayErrorImage } from './displayErrorMessage.js';
+import { displayErrorImage } from "./displayErrorMessage.js";
+
 /**
- * The function `fetchPokemonData` fetches data from the given url using the fetch API.
+ * The function `fetchPokemonData` fetches data from the given URL using the fetch API.
  * If the response is not ok, it will display an error message and throw an error.
- * @param {string} url - The url to fetch data from
- * @returns {Promise<Object>} - The parsed JSON response from the url
+ * @param {string} url - The URL to fetch data from
+ * @returns {Promise<Object>} - The parsed JSON response from the URL
  */
 export async function fetchPokemonData(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok.');
-      }
-      return await response.json();
-    } catch (error) {
-      displayErrorImage()
-      console.error('Error fetching data:', error);
-      throw error;
-     
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
     }
+
+    // Attempt to parse the raw response body as JSON
+    const responseBody = await response.text();
+    if (!responseBody) {
+      throw new Error("Received an empty response from the server.");
+    }
+
+    return JSON.parse(responseBody);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
   }
+}
